c66d2e2076fe519044d204e2bc3fbe25c6450747
piyush-kgp <piyushsinghkgpian@gmail.com>
2019-02-21 05:04:38 +0530

    geest sequence optimized

--- numberphile/musical_sequences.py
+++ numberphile/musical_sequences.py
@@ -37,8 +37,8 @@ def generate_midi_seq(seq, file_name, num_steps = 1000):
     mid.tracks.append(track)
     track.append(Message('program_change', program=12, time=0))
     for i in range(num_steps):
-        track.append(Message('note_on', note=seq[i]%128, velocity=50, time=64))
-        track.append(Message('note_off', note=seq[i]%128, velocity=50, time=64))
+        track.append(Message('note_on', note=seq[i]%128, velocity=50, time=100))
+        track.append(Message('note_off', note=seq[i]%128, velocity=50, time=100))
     mid.save(file_name)
 
 
@@ -74,7 +74,39 @@ def GeestSequence(num_steps=1000):
     return seq
 
 
+@timer
+def GeestSequenceOpt(num_steps=1000):
+    seq=[0]
+    for i in range(num_steps):
+        lst = seq[-1]
+        lst_dgts = str(lst)
+        d_one = lst_dgts[0]
+        if d_one=='9':
+            nxt = 1
+        else:
+            nxt = int(d_one)+1
+        while nxt<=lst:
+            min_dgt_not_in_lst = min([d for d in range(10) if str(d) not in lst_dgts])
+            nxt = int(str(nxt)+str(min_dgt_not_in_lst))
+        seq.append(nxt)
+    return seq
+
+
+def generate_midi_seq_geest(seq, file_name, num_steps = 1000):
+    # Pure sequence
+    seq = seq(num_steps)
+    mid = MidiFile()
+    track = MidiTrack()
+    mid.tracks.append(track)
+    track.append(Message('program_change', program=12, time=0))
+    for i in range(num_steps):
+        track.append(Message('note_on', note=(seq[i]+20)%88, velocity=50, time=80))
+        track.append(Message('note_off', note=seq[i]%128, velocity=50, time=50))
+    mid.save(file_name)
+
+
 if __name__=='__main__':
     generate_midi_seq(file_name='music/recaman.mid', seq=recamanSequence)
     generate_midi_seq(file_name='music/fib.mid', seq=fibonacci)
     generate_midi_seq(file_name='music/geest.mid', seq=GeestSequence, num_steps=60)
+    generate_midi_seq_geest(file_name='music/geest_opt.mid', seq=GeestSequenceOpt, num_steps=2000)
